da4febf1c86fe71e3c62b937b0b785f4f18ef225
JinilSon <dmstnr1320@naver.com>
2022-08-01 15:03:13 +0900

    Service 계층과 Repository 계층의 차이점 주석 처리

--- src/main/java/com/kgd/springbootmall/service/TestService.java
+++ src/main/java/com/kgd/springbootmall/service/TestService.java
@@ -18,3 +18,14 @@ public class TestService {
         return testDto;
     }
 }
+
+
+// DAO와 Repository의 차이점
+// DAO : 데이터 퍼시스턴스의 추상화이다. 테이블 중심의 데이터베이스와 가깝게 고려 된다.(CRUD 제공)
+// Repositoy : 도메인 레이어에 속하는 존재, 객체 컬렉션의 추상화, 여러 DAO를 통해 데이터를 가져와 도메인 객체를 만들어 제공하는 것이 역할이다.
+
+// Service와 Repository의 차이점
+// 본래 Service 계층이 해야하는 기능인 DAO에  접근하는 기능을 모듈화 시킨 것이 Repository이다.
+// 그리하여, DB에 밀접한 접근 기능을 service 계층이 repository에 전부 위임했다 보면 된다.
+
+
