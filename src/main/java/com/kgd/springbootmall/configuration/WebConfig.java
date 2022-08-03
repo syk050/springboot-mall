bd2f09eda429d36b9ca94d8bbaa9ec38f715ff8f
JinilSon <dmstnr1320@naver.com>
2022-08-03 14:45:48 +0900

    autoconfig 구문 추가

--- src/main/java/com/kgd/springbootmall/configuration/WebConfig.java
+++ src/main/java/com/kgd/springbootmall/configuration/WebConfig.java
@@ -1,6 +1,9 @@
 package com.kgd.springbootmall.configuration;//@EnableWebMvc         // 어노테이션 기반 SpringMVC를 구성할 때, 필요한 빈 설정들을 자동으로 해주는 어노테이션(<mvc:annotation-driven/>과 동일역할)
 // but, ViewResolver를 properties파일에서 읽어오는 것과 같은 자동적 boot 기능들은 이 어노테이션을 통해 상실되게 된다.(주의!!)
 
+import com.kgd.springbootmall.SpringbootMallApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +14,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //        @ComponentScan.Filter(type = FilterType.ANNOTATION, value = )
 
         })
+@SpringBootApplication                                    // auto configuration 설정
 public class WebConfig implements WebMvcConfigurer {
+        public static void main(String[] args){
+                SpringApplication.run(SpringbootMallApplication.class, args);
+        }
 
 }
