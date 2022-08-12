7a0b2cc09647b34ebe34b676805aa74b0d569bb7
syk050 <syk050@naver.com>
2022-08-13 01:21:28 +0900

    아이템 상세페이지 생성

---
+++ src/main/frontend/pages/admin/views/ItemsDetail.vue
@@ -0,0 +1,26 @@
+<template>
+<!--  <div class="board-contents">-->
+<!--    <h3>{{ title }}</h3>-->
+<!--    <div>-->
+<!--      <strong class="w3-large">{{ author }}</strong>-->
+<!--      <br>-->
+<!--      <span>{{ createdat }}</span>-->
+<!--    </div>-->
+<!--  </div>-->
+
+<!--  <div class="board-contents">-->
+<!--    <span>{{ contents }}</span>-->
+<!--  </div>-->
+  <h1>Items Detail</h1>
+
+</template>
+
+<script>
+export default {
+  name: "ItemsList"
+}
+</script>
+
+<style scoped>
+
+</style>
