2d7ced9989412649cf4343c3dff1ba7c23cd43fd
syk050 <syk050@naver.com>
2022-08-30 23:05:40 +0900

    결제 페이지 준비 작업

---
+++ src/main/frontend/pages/payment/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>IAMPORT 결제 데모</h2>
+    <button id="iamportPayment" type="button">결제테스트</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ src/main/frontend/pages/payment/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+
+// 가장 먼저 실행되는 자바스크립트 파일,  vue 인스턴스를 생성하는 역할
