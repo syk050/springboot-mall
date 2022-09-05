6be0147a947def286e76506531aa5239dbe49ac3
JinilSon <dmstnr1320@naver.com>
2022-09-05 16:59:41 +0900

    구매가능한 아이템 리스트 show 기능 추가

--- src/main/frontend/pages/search/views/items_view_card.vue
+++ src/main/frontend/pages/search/views/items_view_card.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="items_card_list">
     <div class="first_item">
-      <button class="item_btn"> 아이템을 선택해주세요 </button>
+      <button id="item_btn"> 아이템을 선택해주세요 </button>
     </div>
-    <ul class="items_card_list">
+    <ul id="items_card_list">
       <li>
         <button>
           <img class="items_card_img" src="../../../src/assets/pancakes.jpg">
@@ -23,34 +23,51 @@
 
 <script>
 export default {
-  name: "items_view_card"
+  name: "items_view_card",
+  mounted() {
+    document.getElementById("item_btn").addEventListener("click", ()=> this.show_items_list())
+  },
+  methods:{
+    show_items_list(){
+      document.getElementById("items_card_list").style.visibility = "visible";
+    }
+  }
 }
 </script>
 
 <style scoped>
 
 button{
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
   background-color: transparent;
   border: none;
   border-radius: 4px;
   box-shadow: 0 15px 35px rgba(0, 0, 0, 0.2);
 }
 
-.items_card_list{
+#items_card_list{
   width: 200px;
   display: inline-block;
   text-align: center;
 
 }
 
-.item_btn{
+#item_btn{
 
 }
 
+
+ul#items_card_list{
+  visibility: hidden;
+  list-style: none;
+  margin: 0px;
+  padding: 0px;
+  border: none;
+}
+
+
+
 .items_card_img{
+
   width: 70px;
   height: 80px;
   margin: 10px;
@@ -58,11 +75,5 @@ button{
   border-radius: 6px;
 }
 
-ul.items_card_list{
-  list-style: none;
-  margin: 0px;
-  padding: 0px;
-  border: none;
-}
 
 </style>
