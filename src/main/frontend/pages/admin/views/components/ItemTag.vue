b0da957cf08e2f43a2195a0ce9c9354638f35a02
syk050 <syk050@naver.com>
2022-09-17 20:00:47 +0900

    태그 컴포넌트 상황별 동작 분리

--- src/main/frontend/pages/admin/views/components/ItemTag.vue
+++ src/main/frontend/pages/admin/views/components/ItemTag.vue
@@ -3,7 +3,7 @@
     <h1 class="t-header">Tag</h1>
     <div id="current-tag" class="w3-container w3-bottombar w3-padding-16 dnd-box">
       <!--          현재 태그 -->
-      <span v-for="(tag, idx) in tagList" :key="idx" v-bind:id="tag" class="w3-tag">{{ tag }}</span>
+      <span v-for="(tag, idx) in itemTagList" :key="idx" v-bind:id="tag" class="w3-tag">{{ tag }}</span>
     </div>
     <div id="not-tag" class="w3-container w3-padding-16 dnd-box">
       <!--          추가 가능한 태그 -->
@@ -22,9 +22,9 @@ export default {
   },
   data() {
     return{
-      apiUrl: "/kgd/tag",
+      apiUrl: "/kgd/tag/",
       tags: {},
-      tagList: {},
+      itemTagList: {},
     }
   },
   mounted() {
@@ -37,12 +37,16 @@ export default {
     init() {
       if (this.$route.name === "ItemModify"){  // router에 등록된 name
         console.log("상품 수정 페이지")
-        this.getTag()
+        this.itemTagFilter();
+
       }else if(this.$route.name === "ItemsDetail") {
         console.log("상품 정보 페이지")
-        this.loadTag()
+        this.getItemTag();
+
       }else{
         console.log('상품 등록 페이지')
+        this.loadTag();
+
       }
     },
     async loadTag() {
@@ -55,16 +59,30 @@ export default {
         }
       })
     },
-    getTag() {
+    async getItemTag() {
+      await this.$axios.get(this.apiUrl + this.idxValue)
+          .then(res => {
+            this.itemTagList = res.data.map(x => x.tag)
+          }).catch(err => {
+            if (err.message.indexOf('Network Error') > -1) {
+              alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.')
+            }else{
+              console.error(err);
+            }
+      })
+    },
+    itemTagFilter() {
       this.loadTag()
           .then(() => {
-            let s_tags = new Set(this.tags.map(x => x))
-            let s_tagList = new Set(["app"])
+            this.getItemTag().then(() => {
+              let s_tags = new Set(this.tags.map(x => x))
+              let s_tagList = new Set(this.itemTagList.map(x => x))
 
-            this.tags = new Set([...s_tags].filter(x => !s_tagList.has(x)))
+              this.tags = new Set([...s_tags].filter(x => !s_tagList.has(x)))
+            })
           }).catch(err => {
-            console.error(err);
-          });
+            console.error(err)
+      })
     }
   }
 }
