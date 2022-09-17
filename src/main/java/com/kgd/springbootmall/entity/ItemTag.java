28b84cdfd51d517a2f9ebb518ca3bb758131722b
syk050 <syk050@naver.com>
2022-09-17 20:00:08 +0900

    사용 안하는 Setter 어노테이션 제거

--- src/main/java/com/kgd/springbootmall/entity/ItemTag.java
+++ src/main/java/com/kgd/springbootmall/entity/ItemTag.java
@@ -2,14 +2,12 @@ package com.kgd.springbootmall.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @Entity(name="test_item_tag")
 public class ItemTag {
