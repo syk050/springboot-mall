1be2744f6d5c76f854339f435da88dbe0b40d096
syk050 <syk050@naver.com>
2022-09-23 19:44:31 +0900

    [BE] ItemTag 리스트, 배열 파라미터 수신

--- src/main/java/com/kgd/springbootmall/service/ItemTagService.java
+++ src/main/java/com/kgd/springbootmall/service/ItemTagService.java
@@ -2,16 +2,14 @@ package com.kgd.springbootmall.service;
 
 import com.kgd.springbootmall.dto.ItemTagResponseDto;
 import com.kgd.springbootmall.dto.ItemTagSaveDto;
-import com.kgd.springbootmall.dto.ItemsListResponseDto;
-import com.kgd.springbootmall.dto.ProductDTO;
 import com.kgd.springbootmall.entity.ItemTag;
 import com.kgd.springbootmall.repository.ItemTagRepository;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -42,4 +40,26 @@ public class ItemTagService {
     /** 아이템 태그 등록 */
     @Transactional
     public Long save(ItemTagSaveDto saveDto) { return repository.save(saveDto.toEntity()).getItemId();}
+
+    /** 아이템 태그 리스트 등록 */
+    @Transactional
+    public List<Long> saveList(List<ItemTagSaveDto> saveDtoList) {
+        return repository.saveAll(saveDtoList.stream()
+                .map(ItemTagSaveDto::toEntity)
+                .collect(Collectors.toList())).stream()
+                .map(ItemTag::getItemId)
+                .collect(Collectors.toList());
+    }
+
+    /** 아이템 태그 배열 등록 */
+    @Transactional
+    public List<Long> saveArray(ItemTagSaveDto[] saveDtoArray) {
+        List<ItemTagSaveDto> saveDtoList = Arrays.asList(saveDtoArray);
+        return repository.saveAll(saveDtoList.stream()
+                .map(ItemTagSaveDto::toEntity)
+                .collect(Collectors.toList())).stream()
+                .map(ItemTag::getItemId)
+                .collect(Collectors.toList());
+    }
+
 }
