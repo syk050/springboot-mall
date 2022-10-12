a95bf9ab41b3366770c5bdd43aaa48350925d65c
syk050 <syk050@naver.com>
2022-10-12 21:05:45 +0900

    Revert "iamport-vue-example 가져옴"
    This reverts commit 6a2bdf80d6f489a384a97e2e4751ed4685a65b59.

--- src/main/frontend/pages/payment/main.js
+++ src/main/frontend/pages/payment/main.js
@@ -1,13 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import router from './router';
-import axios from "axios";
+createApp(App).mount('#app')
 
-const app = createApp(App);
-
-app.config.globalProperties.$axios = axios;
-
-app.use(router).mount('#app');
 
 // 가장 먼저 실행되는 자바스크립트 파일,  vue 인스턴스를 생성하는 역할
