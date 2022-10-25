08cf5177578d17f43c8e82cd99afb5cfa549f02b
tjdaud2415 <tjdaud2415@naver.com>
2022-10-25 23:08:34 +0900

    login css

--- src/main/frontend/pages/login/views/Login.vue
+++ src/main/frontend/pages/login/views/Login.vue
@@ -1,14 +1,16 @@
 <template>
   <common-header/>
-  <div>
-    <h2>로그인</h2>
+  <div class="container">
+    <form class="form">
+      <h2 id="log-name">로그인</h2>
+      <input type="text" id="id" placeholder="아이디를 입력해주세요">
+      <input type="password" id="password" placeholder="비밀번호를 입력해주세요">
+      <button type="submit" id="login">
+        <span>로그인</span>
+      </button>
+      <input type="button" id="signup">
+    </form>
   </div>
-  <form>
-    <input type="text" id="id" placeholder="">
-    <input type="password" id="password" placeholder="">
-    <input type="submit" id="login">
-    <input type="button" id="signup">
-  </form>
   <common-footer/>
 </template>
 
@@ -26,5 +28,28 @@ export default {
 </script>
 
 <style>
+  .form{
+    display: grid;
+    place-items: center;
+  }
+  #log-name{
+    font-family: sans-serif;
+  }
+  #id, #password{
+    width: 25%;
+    padding: 16px 15px;
+    margin: 10px 0;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
 
+  #login, #signup{
+    width: 25%;
+    padding: 12px;
+    margin: 10px 0;
+    background-color: #FFE649;
+    border-color: #0B1761;
+    border-radius: 4px;
+  }
 </style>
